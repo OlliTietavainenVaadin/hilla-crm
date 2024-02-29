f9dd894a9b302f57922743a555787cf5760d188a
Olli Tietäväinen <ollit@vaadin.com>
2024-02-29 13:16:50 +0200

    initial commit

--- frontend/views/contacts/ContactsView.tsx
+++ frontend/views/contacts/ContactsView.tsx
@@ -1,4 +1,16 @@
+import ContactRecord from 'Frontend/generated/com/example/application/services/CRMService/ContactRecord';
+import {useEffect, useState} from 'react';
+import {CRMService} from "Frontend/generated/endpoints";
+
 export default function ContactsView() {
+
+    const [contacts, setContacts] = useState<ContactRecord[]>([]);
+    const [selected, setSelected] = useState<ContactRecord | null | undefined>();
+
+    useEffect(() => {
+        CRMService.findAllContacts().then(setContacts);
+    }, []);
+
   return (
     <div className="p-m">
       <h2>Contacts</h2>
